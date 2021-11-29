355dfbed88af6a35ceef508f07b5c2f88c77e795
Tony Ngo <tony@Tonys-MBP.attlocal.net>
2021-11-29 14:12:45 -0500

    changes

--- project.py
+++ project.py
@@ -11,23 +11,40 @@ def main():
     G1 = nx.read_edgelist("seed_G1.edgelist")
     G2 = nx.read_edgelist("seed_G2.edgelist")
 
+    # print(G1.number_of_edges(0,330))
+
+    G1check = list(G1.edges('0'))
+
+    for i in G1check:
+        degreeG1 += 1
+
+    G2check = list(G2.edges('0'))
+
+    for i in G2check:
+        degreeG2 += 1
+
+    print(degreeG1)
+    print(degreeG2)
+
+
+
+    # if (int(G1.edges('0'))):
+    #     degreeG1+= 1
+
+    # print("degree g1 = " + degreeG1)
+    # G = nx.path_graph(4)
+    # G.number_of_edges()
+
     # Read in adjacency list
-    d = {}
-    with open("seed_node_pairs.txt") as f:
-        for line in f:
-            (key, val) = line.split()
-            d[int(key)] = val
+    # d = {}
+    # with open("seed_node_pairs.txt") as f:
+    #     for line in f:
+    #         (key, val) = line.split()
+    #         d[int(key)] = val
+    # print(d)
 
-    print(d)
-
-    # Get list of nodes that aren't connected
-    nodes_connected("a", "d")
-
-    nodes_connected("a", "c")
-
-
-
-
+    # Get list of matched neighbors
+    
 
     # # Check if nodes edges are connected
     # if (nx.is_k_edge_connected(G1, k=1)):
